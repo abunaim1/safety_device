d2ddf865345201ce3a27a4642c1a273cb1987025
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2025-01-02 15:39:08 +0600

    add the host

--- safety_device/settings.py
+++ safety_device/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-q)g0#=acybfapz@(v9oi^7a5z0ibr3jqptt5mbwmru!ti4=+oq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000',
+]
 
 
 # Application definition
